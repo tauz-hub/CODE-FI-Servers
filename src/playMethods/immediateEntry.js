a114d6c0255f754b16743ff378d015239e9ab6c9
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-09-12 13:28:57 -0300

    Update immediateEntry.js

--- src/playMethods/immediateEntry.js
+++ src/playMethods/immediateEntry.js
@@ -7,7 +7,7 @@ export async function immediateEntry(idChannel, client) {
     try {
         let broadcast = null,
             stream = ytdl(url)
-        broadcast = client.voice.createBroadcast();
+        broadcast = await client.voice.createBroadcast();
         broadcast.play(stream)
         const channelReconect = client.channels.cache.get(idChannel) || await client.channels.fetch(idChannel);
         await channelReconect.join();
